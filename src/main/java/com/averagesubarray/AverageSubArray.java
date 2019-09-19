345e5a0e05db9a40d8fea1edf7cfa0d0fcc87415
umesh <umesmishra099@gmail.com>
2019-09-19 09:02:11 +0530

    Averages of subarrays of size k. Slider implementation

--- src/main/java/com/averagesubarray/AverageSubArray.java
+++ src/main/java/com/averagesubarray/AverageSubArray.java
@@ -6,20 +6,23 @@ public class AverageSubArray
 {
   public static void main(String[] args)
   {
-    System.out.println("Averages of subarrays of size k: " + Arrays.toString(findAverages(5, new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2})));
+    System.out.println("Averages of subArrays of size k: " + Arrays.toString(findAverages(5, new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2})));
   }
 
   private static double[] findAverages(int k, int[] arr)
   {
     double[] result = new double[arr.length - k + 1];
-    for (int i = 0; i <= arr.length - k; i++)
+    double sliderSum = 0;
+    int sliderStart = 0;
+    for (int sliderEnd = 0; sliderEnd < arr.length; sliderEnd++)
     {
-      double sum = 0;
-      for (int j = i; j < i + k; j++)
+      sliderSum = sliderSum + arr[sliderEnd];
+      if (sliderEnd >= k - 1)
       {
-        sum = sum + arr[j];
+        result[sliderStart] = sliderSum / k;
+        sliderSum = sliderSum - arr[sliderStart];
+        sliderStart = sliderStart + 1;
       }
-      result[i] = sum / k;
     }
     return result;
   }
